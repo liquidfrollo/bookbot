b3e6ff962a6ebf65e81a0816da98ac53ebe7bb08
liquidfrollo <liquidfrollo@gmail.com>
2024-07-19 13:02:07 -0600

    initial functions

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
+    #print(len(split(file_contents)))
+    print(words(file_contents))
+
+def split(args):
+    words = args.split()
+    return words
+
+def words(args):
+    charlist = {}
+    for char in args:
+        if char not in charlist:
+            charlist[char] = 1
+        else:
+            charlist[char] += 1
+    return charlist
 
 main()
